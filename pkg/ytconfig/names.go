bd1f35202c87a671b6691b5dbe7008acf4727a5e
Aleksandr Razumov <ernado@go-faster.org>
2024-03-08 10:24:39 +0300

    Revert "fix(ytconfig): fix GetHTTPProxiesPort for custom port"
    This reverts commit d0a9ad0854d7aaea6cfa8c370c5284fc6a006ec5.

--- pkg/ytconfig/names.go
+++ pkg/ytconfig/names.go
@@ -127,18 +127,6 @@ func (g *Generator) GetHTTPProxiesServiceName(role string) string {
 	return g.getName(fmt.Sprintf("%s-lb", g.FormatComponentStringWithDefault("http-proxies", role)))
 }
 
-func (g *Generator) GetHTTPProxiesPort(role string) int {
-	for _, proxy := range g.ytsaurus.Spec.HTTPProxies {
-		if proxy.Role != role {
-			continue
-		}
-		if proxy.Transport.HTTPPort != nil {
-			return int(*proxy.Transport.HTTPPort)
-		}
-	}
-	return consts.HTTPProxyHTTPPort
-}
-
 func (g *Generator) GetHTTPProxiesHeadlessServiceName(role string) string {
 	return g.getName(g.FormatComponentStringWithDefault("http-proxies", role))
 }
@@ -148,12 +136,10 @@ func (g *Generator) GetHTTPProxiesStatefulSetName(role string) string {
 }
 
 func (g *Generator) GetHTTPProxiesAddress(role string) string {
-	return fmt.Sprintf("%s.%s.svc.%s:%d",
+	return fmt.Sprintf("%s.%s.svc.%s",
 		g.GetHTTPProxiesServiceName(role),
 		g.ytsaurus.Namespace,
-		g.clusterDomain,
-		g.GetHTTPProxiesPort(role),
-	)
+		g.clusterDomain)
 }
 
 func (g *Generator) GetSchedulerStatefulSetName() string {
