8a7f815d9cccdd5f730bc84e98b83eb37fb90164
tdakkota <tanc13@yandex.ru>
2024-03-19 12:38:29 +0300

    fix(controllers): gracefully handle pod if node is not set yet

--- controllers/node_to_pod_labeller.go
+++ controllers/node_to_pod_labeller.go
@@ -33,14 +33,15 @@ func (l *NodeToPodLabeller) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Handler: admission.HandlerFunc(func(ctx context.Context, req admission.Request) admission.Response {
 			logger := log.FromContext(ctx)
 
-			logger.Info("Handling admission request",
-				"operation", req.Operation,
-			)
 			pod := new(corev1.Pod)
 			if err := decoder.Decode(req, pod); err != nil {
 				return admission.Errored(http.StatusBadRequest, err)
 			}
+
 			nodeName := pod.Spec.NodeName
+			if nodeName == "" {
+				return admission.Allowed("node name is not set yet")
+			}
 
 			node := new(corev1.Node)
 			if err := client.Get(ctx, types.NamespacedName{Name: nodeName}, node); err != nil {
@@ -58,7 +59,7 @@ func (l *NodeToPodLabeller) SetupWebhookWithManager(mgr ctrl.Manager) error {
 					pod.Labels[name] = val
 				}
 			}
-			logger.Info("Patching pod",
+			logger.V(1).Info("Patching pod",
 				"pod", pod.Name,
 				"pod_namespace", pod.Namespace,
 				"node", nodeName,
