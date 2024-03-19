6e0465b4aa8039ea615c1e249a7e20ed4551baa4
tdakkota <tanc13@yandex.ru>
2024-03-19 11:24:17 +0300

    feat(controllers): add webhook to copy node labels to pod

---
+++ controllers/node_to_pod_labeller.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type NodeToPodLabeller struct {
+	Labels map[string]struct{}
+}
+
+//+kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups=cluster.ytsaurus.tech,resources=pods,verbs=create;update,versions=v1,name=yt-pod-rack.kb.io,admissionReviewVersions=v1
+
+const apiPathMutatePod = "/mutate-v1-pod"
+
+func (l *NodeToPodLabeller) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	client := mgr.GetClient()
+
+	decoder, err := admission.NewDecoder(mgr.GetScheme())
+	if err != nil {
+		return fmt.Errorf("create decoder: %w", err)
+	}
+
+	mgr.GetWebhookServer().Register(apiPathMutatePod, &admission.Webhook{
+		Handler: admission.HandlerFunc(func(ctx context.Context, req admission.Request) admission.Response {
+			logger := log.FromContext(ctx)
+
+			pod := new(corev1.Pod)
+			if err := decoder.Decode(req, pod); err != nil {
+				return admission.Errored(http.StatusBadRequest, err)
+			}
+			nodeName := pod.Spec.NodeName
+
+			node := new(corev1.Node)
+			if err := client.Get(ctx, types.NamespacedName{Name: nodeName}, node); err != nil {
+				logger.Error(err, "get node to patch", "nodeName", nodeName)
+				return admission.Errored(http.StatusInternalServerError, err)
+			}
+
+			for name := range l.Labels {
+				if val, ok := node.Labels[name]; ok {
+					if pod.Labels == nil {
+						pod.Labels = map[string]string{}
+					}
+					pod.Labels[name] = val
+				}
+			}
+
+			marshaledPod, err := json.Marshal(pod)
+			if err != nil {
+				return admission.Errored(http.StatusInternalServerError, err)
+			}
+			return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+		}),
+	})
+
+	return nil
+}
